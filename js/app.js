c540c94891da223b0a1ba77c325f0e2a05182a08
olatunji miles <olatunjimiles094@gmail.com>
2020-05-20 11:32:31 -0700

    added js

--- js/app.js
+++ js/app.js
@@ -1,38 +1,58 @@
-let el = el => document.querySelector(el);
+// document.getElementById("cust")
+// document.getElementsByClassName();
+// document.getElementsByTagName("p")
+// document.body
+
+let el = (el) => document.querySelector(el);
 let bar = el("#customProgressbar").querySelector(".progress-bar");
 let fill = el("#fill");
 let clear = el("#clear");
-// fill.addEventListener("click", (cb) => {
-//     switch(bar.style.width){
-//         case "0%":
-//             bar.style.width = "25%";
-//             bar.textContent = "25%";
-//             break;
-//         case "25%":
-//             bar.style.width = "50%";
-//             bar.textContent = "50%";
-//             break;
-//         case "50%":
-//             bar.style.width = "75%";
-//             bar.textContent = "75%";
-//             break;
-//         case "75%":
-//             bar.style.width = "100%";
-//             bar.textContent = "100%";
-//             break;
-//         default: false;
-//     }
-// })
-// clear.addEventListener("click", () => bar.style.width = "0%"); 
 
-// fill.addEventListener("click", (cb) => {
-//     console.log(cb)
-    
-//     for(let i = 0; i < 100; i++){
-//         setInterval(function(){
-//             console.log(i)
-//             bar.style.width = i+"%";
-//             bar.textContent = i+"%";
-//         }, 500)
-//     }
-// })
+let i = 0;
+fill.addEventListener("click", (cb) => {
+  let interval = setInterval(timer, 100);
+
+  function timer() {
+    i++;
+    if (i < 100) {
+      bar.style.width = i + "%";
+      bar.textContent = i + "%";
+
+      if (i === 99) {
+        setTimeout(function () {
+          bar.style.width = "100%";
+          bar.textContent = "100%";
+          bar.style.backgroundColor = "green";
+          bar.textContent = "Upload Completed";
+        }, 3000);
+      }
+    }
+  }
+
+  clear.addEventListener("click", () => {
+    clearInterval(interval);
+    i = 0;
+    bar.style.width = "0%";
+    bar.textContent = "0%";
+    bar.style.backgroundColor = "#007bff";
+  });
+});
+
+(function () {
+  setInterval(() => {
+    el("#clock").textContent = new Date().toLocaleTimeString();
+  }, 1000);
+})();
+
+var colors = ["red", "blue", "yellow"];
+let index = Math.floor(Math.random() * 3 + 1);
+setInterval(() => {
+    console.log(index)
+    console.log(colors[index])
+}, 300)
+
+// Implement a pause function
+// Implement random colours
+// read about AJAX (asynchronous javascript and XML)
+// read about fetch API
+// read more about functions, Objects, Arrays, Closure, Lexical Scope, recursion, Math
